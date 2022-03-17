f78d1714f10a13649be3f152634bd2fba021f938
allebedev <westwardbukle5@gmail.com>
2022-03-17 16:31:22 +0300

    Migrations

---
+++ Sixgram.Stories.Database/Migrations/20220317132759_Add_FileId.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Sixgram.Stories.Database.Migrations
+{
+    public partial class Add_FileId : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "StoriesModels",
+                type: "character varying(100)",
+                maxLength: 100,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "FileId",
+                table: "StoriesModels",
+                type: "uuid",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FileId",
+                table: "StoriesModels");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "StoriesModels",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "character varying(100)",
+                oldMaxLength: 100);
+        }
+    }
+}
